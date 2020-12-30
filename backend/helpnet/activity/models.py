4e19121a333a6d9ee0868da7ae7cfded27b72f65
Victor Inojosa <vijoin@gmail.com>
2020-12-30 20:15:23 -0300

    Add initial version for Activity model

--- backend/helpnet/activity/models.py
+++ backend/helpnet/activity/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+
+    name = models.CharField(max_length=100)
+    project_id = models.CharField(max_length=100)  # ToDo: convert to m2o
+    date = models.DateTimeField(auto_now_add=True)
+    leader_id = models.CharField(max_length=100)  # ToDo: convert to m2o
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "activities"
