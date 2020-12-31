53e72cfac2fadac11651dda25aee9cad4ac78977
Victor Inojosa <vijoin@gmail.com>
2020-12-31 10:26:45 -0300

    Use api_view decorator to define to views to proccess the requests

--- backend/helpnet/activity/views.py
+++ backend/helpnet/activity/views.py
@@ -1,49 +1,47 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Activity
 from .serializers import ActivitySerializer
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def activity_list(request):
 
     if request.method == 'GET':
         activities = Activity.objects.all()
         serializer = ActivitySerializer(activities, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ActivitySerializer(data=data)
+        serializer = ActivitySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def activity_detail(request, id):
     try:
         activity = Activity.objects.get(pk=id)
 
     except Activity.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ActivitySerializer(activity)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ActivitySerializer(activity, data=data)
+        serializer = ActivitySerializer(activity, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         activity.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     else:
-        return HttpResponse(status=405)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
