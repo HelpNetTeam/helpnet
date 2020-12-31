8e240c9f8d6a42ff4cca3888fb99185444cc4ab2
Victor Inojosa <vijoin@gmail.com>
2020-12-30 22:29:23 -0300

    Get details for a specific activity. Also update and delete it Using functions based views

--- backend/helpnet/activity/urls.py
+++ backend/helpnet/activity/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import activity_list
+from .views import activity_list, activity_detail
 
 urlpatterns = [
-    path('activity', activity_list)
+    path('activity', activity_list),
+    path('activity/<int:id>', activity_detail)
 ]
--- backend/helpnet/activity/views.py
+++ backend/helpnet/activity/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -20,3 +19,31 @@ def activity_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def activity_detail(request, id):
+    try:
+        activity = Activity.objects.get(pk=id)
+
+    except Activity.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ActivitySerializer(activity)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ActivitySerializer(activity, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        activity.delete()
+        return HttpResponse(status=204)
+
+    else:
+        return HttpResponse(status=405)
