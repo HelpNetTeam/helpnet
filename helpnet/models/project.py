7868aa107c6e68e17732ed50f9bdaaf8ab707d45
Victor Inojosa <vijoin@gmail.com>
2021-01-29 19:24:45 -0300

    [ADD] helpnet: Add initial version of the module with main models

---
+++ helpnet/models/project.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+
+class HelpnetProject(models.Model):
+    _name = 'helpnet.project'
+
+    name = fields.Char()
+    organization_id = fields.Many2one('helpnet.organizations')
+    responsible_id = fields.Many2one('res.partner')
+    website = fields.Char()
