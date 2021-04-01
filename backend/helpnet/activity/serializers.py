8653ef8367184f05360df7c2e7aeea8ed5938e2d
Victor Inojosa <vijoin@gmail.com>
2021-04-01 17:56:04 -0300

    Remove old Django app

--- backend/helpnet/activity/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Activity
-
-
-class ActivitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Activity
-        fields = ['id', 'name', 'project_id', 'leader_id', 'date']
--- backend/helpnet/activity/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ActivityList, ActivityDetails
-
-urlpatterns = [
-    path('activity', ActivityList.as_view()),
-    path('activity/<int:id>', ActivityDetails.as_view()),
-]
