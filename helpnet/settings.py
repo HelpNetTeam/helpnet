7c66d6e3561c92194cda4c180d8517b1cf455890
Victor Inojosa <vijoin@gmail.com>
2021-04-16 02:16:43 +0000

    Collect static files to be served

--- helpnet/settings.py
+++ helpnet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y!ky72zbs%&*8-!kpip*pki2vqsuyv4daiw(pnk-+)@p!$qr8_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['96.126.120.66', 'localhost']
 
 
 # Application definition
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 TAGGIT_CASE_INSENSITIVE = True
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
