aea47d5de0dba3a5bbebedd6662d5ae7e1fb2c9e
Victor Inojosa <vijoin@gmail.com>
2021-04-18 09:42:56 -0300

    Improve readability on activity tests

--- activity/tests/test_activity.py
+++ activity/tests/test_activity.py
@@ -12,23 +12,21 @@ client = Client()
 class ActivityTestCase(TestCase):
     def setUp(self):
 
-        Activity.objects.create(name="Activity1", date='2021-12-31 09:00:00', latitude=0.0, longitude=0.0)
-        Activity.objects.create(name="Activity2", date='2021-12-31 21:00:00', latitude=0.1, longitude=0.1)
+        self.activity1 = Activity.objects.create(
+            name="Activity1", date='2021-12-31 09:00:00', latitude=0.0, longitude=0.0)
+        self.activity2 = Activity.objects.create(
+            name="Activity2", date='2021-12-31 21:00:00', latitude=0.1, longitude=0.1)
 
     def test_activity_creation(self):
         """Activities are being properly created"""
-        activity1 = Activity.objects.get(name="Activity1")
-        activity2 = Activity.objects.get(name="Activity2")
-        self.assertEqual(activity1.name, 'Activity1')
-        self.assertEqual(activity2.name, 'Activity2')
+        self.assertEqual(self.activity1.name, 'Activity1')
+        self.assertEqual(self.activity2.name, 'Activity2')
 
     def test_get_valid_single_activity(self):
-        activity1 = Activity.objects.get(name="Activity1")
         response = client.get(
-            reverse('activity-detail', kwargs={'pk': activity1.pk})
+            reverse('activity-detail', kwargs={'pk': self.activity1.pk})
             )
-        activity = Activity.objects.get(pk=activity1.pk)
-        serializer = ActivitySerializer(activity)
+        serializer = ActivitySerializer(self.activity1)
         # self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -53,14 +51,12 @@ class ActivityTestCase(TestCase):
 
     def test_activity_likes_count(self):
         """Activity likes are being computed and returned OK"""
-        activity1 = Activity.objects.get(name="Activity1")
-        self.assertTrue(0 <= activity1.likes_count <= 100)
+        self.assertTrue(0 <= self.activity1.likes_count <= 100)
     
     def test_activity_comments_count(self):
         """Activity comments are being computed and returned OK"""
-        activity1 = Activity.objects.get(name="Activity1")
-        self.assertTrue(0 <= activity1.comments_count <= 100)
-        # self.assertTrue(0 <= activity1.get_comments_count() <= 100)
+        self.assertTrue(0 <= self.activity1.comments_count <= 100)
+        # self.assertTrue(0 <= self.activity1.get_comments_count() <= 100)
 
 ## Test Comments
 
